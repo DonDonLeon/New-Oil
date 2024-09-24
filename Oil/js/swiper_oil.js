52c71a8be7bdf84391d772031561c1b5a09d69e2
Leonid <87618658+DonDonLeon@users.noreply.github.com>
2024-09-25 01:02:35 +0300

    Add files via upload

---
+++ Oil/js/swiper_oil.js
@@ -0,0 +1,11 @@
+var swiperOil = new Swiper(".swiperOil", {
+  loop: true,
+  speed: 10000,
+  slidesPerView: "auto",
+  autoplay: {
+    enabled: true,
+    delay: 2,
+    pauseOnMouseEnter: false,
+    disableOnInteraction: true,
+  },
+})
